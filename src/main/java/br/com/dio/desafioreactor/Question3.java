eb19b851eaf84f8c145b1a29cfb92c85ef7814ea
Jean Carlos <jeands203@gmail.com>
2026-03-02 22:24:45 -0300

    feat: first try exercise

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,10 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
-
+    public Mono<Void> userIsValid(final User user) {
+        if (user.password() == null || user.password().length() <= 8) {
+            return Mono.error(new IllegalArgumentException("Senha muito curta!"));
+        }
+        return Mono.empty();
+    }
 }
